29819126cf5a11b0c0c5802ec128e46dc70f955c
Nathan Liefting <nathan@oicts.nl>
2023-03-11 08:28:09 +0100

    Initial files + add Chapter 7

---
+++ chapter10/api_test.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+#API login information
+url = "http://10.16.16.152/zabbix/api_jsonrpc.php"
+api_token = "PUT_YOUR_TOKEN_HERE"
+
+#Add new code below here
+
+
+
+#Add new code above here
+
+#Generate hosts
+zabbix_hosts = get_hosts(api_token,url)
+generate_host_file(zabbix_hosts,"/home/results")
